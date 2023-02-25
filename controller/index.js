a2f04797095bb1f9316dde4619a356d634c7632f
santhoshkumarampolum <santhoshkumarampolu@gmail.com>
2023-02-25 22:09:29 +0530

    Added nodemon

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,3 @@
-expors.index = (req, res, next) =>{
+exports.index = function (req, res, next) {
     res.render('index', { title: 'Express' });
   }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 let index = require('../controller/index')
 /* GET home page. */
-router.get('/',index );
+router.get('/',index.index );
 
 module.exports = router;
